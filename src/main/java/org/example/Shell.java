d9f8c20ca424296f7c2238f672c344b686927c63
HouBinyang <21302010042@m.fudan.edu.cn>
2023-10-13 20:27:21 +0800

    Commands: Shell重构

--- src/main/java/org/example/Shell.java
+++ src/main/java/org/example/Shell.java
@@ -24,123 +24,146 @@ public class Shell {
         System.out.print(">>>");
 
         //初始化命令，调用者，接收者
-        Command command = null;
+        Command command;
         Invoker invoker = new Invoker();
         Document document = new Document();
         //日志及统计模块
         HistoryLog historyLog = new HistoryLog();
         StatsLog statsLog = new StatsLog();
 
-        //start log
-        historyLog.ses_start_log();
-        statsLog.ses_start_log();
-
+        start_log(historyLog, statsLog);
 
         Scanner scanner = new Scanner(System.in);
-
-        do{
+        do {
             //命令及参数
             String cmd;
-            List<String> cmd_args =new ArrayList<>();
+            List<String> cmd_args = new ArrayList<>();
 
             //获取命名和参数并执行对应的操作
-            Scanner cmd_scanner =new Scanner(scanner.nextLine());
-            if(cmd_scanner.hasNext()){
+            Scanner cmd_scanner = new Scanner(scanner.nextLine());
+            if (cmd_scanner.hasNext()) {
                 cmd = cmd_scanner.next();
-                while (cmd_scanner.hasNext()){
+                while (cmd_scanner.hasNext()) {
                     cmd_args.add(cmd_scanner.next());
                 }
 
-                if(cmd.equals("quit")){
-                    //quit log
-                    if(StatsLog.current_file!=null){
-                        statsLog.file_end_log();
-                    }
-                    statsLog.ses_end_log();
+                //程序结束标志
+                if (cmd.equals("quit")) {
+                    quit_log(statsLog);
                     System.out.println("Bye~");
                     System.exit(0);
                 }
-                command = getCommand(cmd, cmd_args,document,historyLog,statsLog);
+
+                command = getCommand(cmd, cmd_args, document, historyLog, statsLog);
                 invoker.setCommand(command);
                 boolean opt_success = invoker.execCommand();
 
-                if(opt_success){
-                    //cmd log
-                    historyLog.cmd_log(cmd,cmd_args);
-                    if(cmd.equals("load")){
-                        if(StatsLog.current_file!=null){
-                            statsLog.file_end_log();
+                //操作成功则计入日志
+                if (opt_success) {
+                    cmd_log(historyLog, cmd, cmd_args);
+                    if (cmd.equals("load")) {
+                        if (StatsLog.current_file != null) {
+                            file_end_log(statsLog);
                         }
-                        statsLog.file_start_log(Document.file_path);
+                        file_start_log(statsLog);
                     }
                 }
-
-                System.out.print(">>>");
             } else {
-              System.out.println("输入为空!");
-              System.out.print(">>>");
+                System.out.println("输入为空!");
             }
+            System.out.print(">>>");
+
         }
         while (scanner.hasNextLine());
     }
 
 
-    private static Command getCommand(String cmd, List<String> cmd_args,Document document,HistoryLog historyLog,StatsLog statsLog) {
+    private static Command getCommand(String cmd, List<String> cmd_args, Document document, HistoryLog historyLog, StatsLog statsLog) {
         Command command;
         switch (cmd) {
             case "load" -> {
-                if(cmd_args.size()>0){
+                if (cmd_args.size() > 0) {
                     command = new LoadCommand(cmd_args, document);
-                }else {
+                } else {
                     command = new NothingCommand();
-                    System.out.println(cmd +"参数不能为空");
+                    System.out.println(cmd + "参数不能为空");
                 }
             }
             case "save" -> command = new SaveCommand(document);
             case "insert" -> {
-                if(cmd_args.size()>0){
+                if (cmd_args.size() > 0) {
                     command = new InsertCommand(cmd_args, document);
-                }else {
+                } else {
                     command = new NothingCommand();
-                    System.out.println(cmd +"参数不能为空");
+                    System.out.println(cmd + "参数不能为空");
                 }
             }
             case "append-head" -> {
-                if(cmd_args.size()>0){
+                if (cmd_args.size() > 0) {
                     command = new AppendHeadCommand(cmd_args, document);
-                }else {
+                } else {
                     command = new NothingCommand();
-                    System.out.println(cmd +"参数不能为空");
+                    System.out.println(cmd + "参数不能为空");
                 }
             }
             case "append-tail" -> {
-                if(cmd_args.size()>0){
+                if (cmd_args.size() > 0) {
                     command = new AppendTailCommand(cmd_args, document);
-                }else {
+                } else {
                     command = new NothingCommand();
-                    System.out.println(cmd +"参数不能为空");
+                    System.out.println(cmd + "参数不能为空");
                 }
             }
             case "delete" -> {
-                if(cmd_args.size()>0){
+                if (cmd_args.size() > 0) {
                     command = new DeleteCommand(cmd_args, document);
-                }else {
+                } else {
                     command = new NothingCommand();
-                    System.out.println(cmd +"参数不能为空");
+                    System.out.println(cmd + "参数不能为空");
                 }
             }
             case "list" -> command = new ListCommand(document);
-            case "history" -> {
-                command = new HistoryCommand(cmd_args,historyLog);
-            }
-            case "stats" -> {
-                command = new StatsCommand(cmd_args,statsLog);
-            }
+            case "history" -> command = new HistoryCommand(cmd_args, historyLog);
+            case "stats" -> command = new StatsCommand(cmd_args, statsLog);
             default -> {
-                System.out.println(cmd +" 不是有效命令");
+                System.out.println(cmd + " 不是有效命令");
                 command = new HelpCommand();
             }
         }
         return command;
+    }
+
+    private static void start_log(HistoryLog historyLog, StatsLog statsLog) throws IOException {
+        //start log
+        historyLog.ses_start_log();
+        statsLog.ses_start_log();
+        //start log
+    }
+
+    private static void quit_log(StatsLog statsLog) throws IOException {
+        //quit log
+        if (StatsLog.current_file != null) {
+            file_end_log(statsLog);
+        }
+        statsLog.ses_end_log();
+        //quit log
+    }
+
+    private static void file_start_log(StatsLog statsLog) {
+        //file_start log
+        statsLog.file_start_log(Document.file_path);
+        //file_start log
+    }
+
+    private static void file_end_log(StatsLog statsLog) {
+        //file_end log
+        statsLog.file_end_log();
+        //file_end log
+    }
+
+    private static void cmd_log(HistoryLog historyLog, String cmd, List<String> cmd_args) throws IOException {
+        //cmd log
+        historyLog.cmd_log(cmd, cmd_args);
+        //cmd log
     }
 }
